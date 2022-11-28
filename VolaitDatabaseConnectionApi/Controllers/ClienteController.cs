d50995275508a469ebf033ab567d1248f64293cf
BrnBastos <bruninhosbastos0228@gmail.com>
2022-11-27 23:20:23 -0300

    Adicionar arquivos de projeto.

---
+++ VolaitDatabaseConnectionApi/Controllers/ClienteController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using VolaitDatabaseConnectionApi.Models;
+using VolaitDatabaseConnectionApi.Models.DAO;
+using VolaitDatabaseConnectionApi.Utils;
+
+namespace VolaitDatabaseConnectionApi.Controllers
+{
+    public class ClienteController : ApiController
+    {
+        // GET: api/Cliente
+        public List<Cliente> Get()
+        {
+            var clienteDAO = new ClienteDAO();
+            var clienteList = clienteDAO.SelectAllClientes();
+            return clienteList;
+        }
+
+        // POST: api/Cliente
+        [HttpPost]
+        public void Post([FromBody] Cliente cliente)
+        {
+            var clienteDAO = new ClienteDAO();
+            clienteDAO.SaveCliente(cliente);
+        }
+
+        [HttpGet]
+        public Cliente ConsultarCliente(string login)
+        {
+            ClienteDAO dao = new ClienteDAO();
+            var cliente = dao.ConsultarCliente(login);
+            return cliente;
+        }
+
+        // // EDITAR USUÁRIO
+        // [System.Web.Http.HttpPut]
+        // public void EditarUsuario([FromBody] UsuarioViewModel vmUsuario)
+        // {
+        //     if (vmUsuario == null)
+        //         throw new HttpResponseException(new HttpResponseMessage(HttpStatusCode.NotFound));
+
+        //     UsuarioDAO dao = new UsuarioDAO();
+        //     dao.editarUsuario(vmUsuario);
+        // }
+
+        // POST: api/Cliente
+        //[HttpPost]
+        //public Cliente Login([FromBody]Cliente clienteUsuario)
+        //{
+        //    ClienteDAO clientedao = new ClienteDAO();
+        //    var cliente = clientedao.LoginCliente(clienteUsuario);
+
+        //    if (cliente == null | cliente.LoginCliente != clienteUsuario.LoginCliente)
+        //    {
+        //        //return "Login incorreta";
+
+        //    }
+
+        //    if (cliente.SenhaCliente != Hash.GerarHash(clienteUsuario.SenhaCliente))
+        //    {
+        //        cliente.SenhaCliente = "erro meno";
+        //        return cliente;
+        //    }
+        //    return cliente;
+        //}
+    }
+}
---
+++ VolaitDatabaseConnectionApi/Models/Cupom.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VolaitDatabaseConnectionApi.Models
+{
+    public class Cupom
+    {
+        public int CupomID { get; set; }
+        public string CupomCode { get; set; }
+        public decimal ValorDesconto { get; set; }
+        public DateTime CupomValidade { get; set; }
+
+        public Cupom(int CupomID_, string CupomCode_, decimal ValorDesconto_, DateTime CupomValidade_)
+        {
+            this.CupomID = CupomID_;
+            this.CupomCode = CupomCode_;
+            this.ValorDesconto = ValorDesconto_;
+            this.CupomValidade = CupomValidade_;
+        }
+    
+        public Cupom()
+        {
+
+        }
+    }
+}
